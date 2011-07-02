125574961570df25a18164cbce5dd4fd851e7fb2
kibo <tomasjurman@gmail.com>
2011-07-02 20:39:30 +0200

    topten list

--- js/dictionary.js
+++ js/dictionary.js
@@ -9,8 +9,8 @@ function Dictionary(){
     this.idx = function () {
             var n = 0;            
             return {
-                next: function() { (n >= ( Dictionary.prototype.raw.length - 1) ) ? n = 0 :  n++; },
-                get: function() { return n; }
+                next: function() {(n >= ( Dictionary.prototype.raw.length - 1) ) ? n = 0 :  n++;},
+                get: function() {return n;}
             };              
     }();  
        
@@ -51,3 +51,34 @@ Dictionary.prototype.shuffle = function(o) {
 Dictionary.prototype.random = function(){
    return Math.round( Math.random() * 2 );
 };
+
+Dictionary.prototype.saveResults = function( bonus, name ){            
+    var results =  localStorage.getItem("results") != null ?
+                        JSON.parse( localStorage.getItem("results") ) :
+                        {};
+    results[bonus] = name;
+    localStorage.setItem( "results", JSON.stringify(results) );                                    
+};
+
+Dictionary.prototype.getSortedKeys = function(){        
+    var results =  JSON.parse( localStorage.getItem("results") );   
+    var keys = [];      
+    for(var key in results){
+        keys.push(key);
+    }
+    
+    keys.sort(); 
+    keys.reverse();
+    return keys;       
+};
+
+Dictionary.prototype.checkResults = function(){ 
+    var MAX_ITEM = 10;
+    var keys = Dictionary.prototype.getSortedKeys();    
+    if(keys.length > MAX_ITEM){
+        results =  JSON.parse( localStorage.getItem("results") );  
+        delete results[ keys[ keys.length-1 ] ];
+        localStorage.setItem( "results", JSON.stringify(results) );                   
+    } 
+}
+
